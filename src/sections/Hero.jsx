df0e185a0aa2204e16b0334145f2bce8882765d2
Utkrist Jaiswal <utkrist.j@gmail.com>
2025-04-11 23:51:07 +0530

    Added 3D model and Animated Counter along with button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,26 @@
 import { words } from "../constants/index.js"
 import Button from "../components/button.jsx"
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-text h1',
+            {
+                y: 50,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.3,
+                duration: 1,
+                ease: 'power2.inOut',
+            }
+        )
+    })
   return (
     <section id="hero" className='relative overflow-hidden'>
         <div className='absolute top-0 left-0 z-10'>
@@ -49,11 +67,12 @@ const Hero = () => {
             </header>
             {/* Right: 3D Model*/}
             <figure>
-                <div className="hero-3d-layout ">
+                <div className="hero-3d-layout">
                     <HeroExperience/>
                 </div>
             </figure>
         </div>
+        <AnimatedCounter/>
     </section>
   )
 }
